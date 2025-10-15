b0dbf5d518b918c44b3ad804ad9cc5ff0d3d6dfe
Daniel Bielech <dacfnielyt@gmail.com>
2025-10-15 12:05:44 +0100

    Caching getAllEmployees to improve performance

--- api/src/main/java/com/reliaquest/api/service/EmployeeService.java
+++ api/src/main/java/com/reliaquest/api/service/EmployeeService.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 /**
@@ -33,6 +34,7 @@ public class EmployeeService {
     /**
      * Responsible for fetching all employees from the mock API
      */
+    @Cacheable("employees")
     public List<Employee> getAllEmployees() {
         logger.info("Service: Fetching all employees");
         List<Employee> employees = apiClient.fetchAllEmployees();
